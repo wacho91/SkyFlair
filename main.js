30d5512a8511116835cd6cbb98bffb7afaa8ab58
wacho91 <criskol.71@gmail.com>
2024-09-06 11:47:32 -0500

    Navbar Complete

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const navLinks = document.getElementById("nav-links");
 const menuBtnIcons = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e) => {
-    e.preventDefault();
     navLinks.classList.toggle("open");
 
     const isOpen = navLinks.contains("open");
     menuBtnIcons.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcons.setAttribute("class", "ri-menu-line");
 });
