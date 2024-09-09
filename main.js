63de7c29b7250966006fa4bc7bb26715546f59db
wacho91 <criskol.71@gmail.com>
2024-09-09 10:23:30 -0500

    Scroll revear Header

--- main.js
+++ main.js
@@ -24,3 +24,18 @@ ScrollReveal().reveal(".header__image img", {
     ...scrollRevealOption,
     origin: "right"
 })
+
+ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__btns", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
